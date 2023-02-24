b79e6a3657cbeb61f990194d07d9f6e66700cc31
Matt Vaerewyck (Macbook) <mvaerewyck@gmail.com>
2023-02-24 17:41:11 -0500

    trying to get regular expression to work

--- WordleDic.py
+++ WordleDic.py
@@ -2,31 +2,50 @@
 # This code will:
 #
 # find the 5-letter words from pydictionary and store them
-# deal with interface from WordleSolver and cut down the stored list
+# deal with interface from WordleSolver and cut dworlown the stored list
 
 # import statements
 
-#import pyDictionary as pyDic 
+
+# stdlib
+import re
+import requests
+
+#from PyDictionary import PyDictionary as pyDic
 
 # Construct Method
-class WordleDic:
-    def _init_():
+class wordleSolve:
+    def getWordList(self):
+        wordlist = []
         # Get 5-letter words
- #       wordList = pyDic.pyDictionary()
-         
+                
+        
+        # get list of five-letter words from meaningpedia.com
+        # https://en.wikipedia.org/wiki/Lists_of_English_words#External_links
+        meaningpedia_resp = requests.get("https://meaningpedia.com/5-letter-words?show=all")
 
-        pass
+        # get list of words by grabbing regex captures of response
+        # compile regex
+        pattern = re.compile(r'<span itemprop="name">(\w+)</span>')
+        
+        # find all matches
+        pattern_list = pattern.findall(meaningpedia_resp.text) 
+
+        
+        for result in pattern_list:
+            wordlist.append(result)
+
+        self.word_list = wordlist.split
+    
     def wordsThatRemain():
         # use feedback to reduce the overall dictionary to 
         # make a list of words make a suggestion
 
         pass
+        
+    def wordleFeedback(userGuess,wordleReturn):
 
-    def wordleFeedback(feedbackString):
-
-        # get input back from user regarding colors
-        # . is a miss
-        # y is a yellow letter
-        # g is a green letter
-
+        # use feedback from wordle to remove potential words from
+        # the list
+        
         pass
