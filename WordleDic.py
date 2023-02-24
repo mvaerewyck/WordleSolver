7b3d5f8f0e3a49bbb872cc463cc05efca8e231f5
Matthew V (Chromebook) <mvaerewyck@gmail.com>
2023-02-24 16:09:58 -0500

    Initial Layout

---
+++ WordleDic.py
@@ -0,0 +1,32 @@
+# constructor for WordleSolver.py
+# This code will:
+#
+# find the 5-letter words from pydictionary and store them
+# deal with interface from WordleSolver and cut down the stored list
+
+# import statements
+
+import pyDictionary as pyDic 
+
+# Construct Method
+class WordleDic:
+    def _init_():
+        # Get 5-letter words
+        wordList = pyDic.pyDictionary()
+         
+
+
+    def wordsThatRemain():
+        # use feedback to reduce the overall dictionary to 
+        # make a list of words make a suggestion
+
+        pass
+
+    def wordleFeedback(feedbackString):
+
+        # get input back from user regarding colors
+        # . is a miss
+        # y is a yellow letter
+        # g is a green letter
+
+        pass
