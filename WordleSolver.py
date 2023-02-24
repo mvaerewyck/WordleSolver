46e90e19e04505e8b5d5fcdfd2fe61a1e413f1c6
Matthew V (Chromebook) <mvaerewyck@gmail.com>
2023-02-24 16:10:47 -0500

    Init layout

---
+++ WordleSolver.py
@@ -0,0 +1,16 @@
+## Wordle Solver
+# Matthew Vaerewyck
+# Started: 24 Feb 2023
+
+import WordleDic
+
+# handles user input and reports back and forth to WordleDic.py
+
+userGuess = "slate"     # user's next guess, initialized as "slate"
+availWords = {""}     # dictionary list of available words
+wordleReturn = "....."  # this will be the user's feedback from wordle
+
+
+
+
+
