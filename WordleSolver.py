87f57864d05f157b51595cbeb5f1796780b8adff
Matt Vaerewyck (Macbook) <mvaerewyck@gmail.com>
2023-02-24 17:42:49 -0500

    Trying to get regular expression to work

--- WordleSolver.py
+++ WordleSolver.py
@@ -2,7 +2,7 @@
 # Matthew Vaerewyck
 # Started: 24 Feb 2023
 
-import WordleDic
+from WordleDic import wordleSolve
 
 # handles user input and reports back and forth to WordleDic.py
 
@@ -10,6 +10,14 @@ userGuess = "slate"     # user's next guess, initialized as "slate"
 availWords = {""}     # dictionary list of available words
 wordleReturn = "....."  # this will be the user's feedback from wordle
 
+print('-- Fetching word list -- ')
+Wordle = wordleSolve.getWordList
+
+print("You guessed first: ", userGuess)
+wordleReturn = input("Please input the response from Wordle (using yg.) :")
+
+
+wordleSolve.wordleFeedback(userGuess,wordleReturn)
 
 
 
