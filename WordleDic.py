879cee1837bf857fd5cba0b3bb2ce6b4705964d8
Matt Vaerewyck (Macbook) <mvaerewyck@gmail.com>
2023-02-27 00:35:48 -0500

    Making progress.
    Can now pull down word list, parse guess, parse feedback.
    
    Todo: use feedback to reduce words

--- WordleDic.py
+++ WordleDic.py
@@ -5,50 +5,85 @@
 # deal with interface from WordleSolver and cut dworlown the stored list
 
 # import statements
-
-
-# stdlib
-import re
-import requests
-
-#from PyDictionary import PyDictionary as pyDic
+import re, requests
 
 # Construct Method
-class wordleSolve:
+class wordleSolve():
+
+    g_list = []
+    y_list = []
+    x_list = []
+
     def getWordList(self):
+
+        #############################################################
+        ## pulls a list of 5 letter words from the web
+        #############################################################
         wordlist = []
         # Get 5-letter words
-                
-        
+
         # get list of five-letter words from meaningpedia.com
         # https://en.wikipedia.org/wiki/Lists_of_English_words#External_links
-        meaningpedia_resp = requests.get("https://meaningpedia.com/5-letter-words?show=all")
+        wordlist_webpage = requests.get('https://meaningpedia.com/5-letter-words?show=all')
 
         # get list of words by grabbing regex captures of response
         # compile regex
         pattern = re.compile(r'<span itemprop="name">(\w+)</span>')
-        
-        # find all matches
-        pattern_list = pattern.findall(meaningpedia_resp.text) 
 
-        
+        # find all matches
+        pattern_list = pattern.findall(wordlist_webpage.text)
+
         for result in pattern_list:
             wordlist.append(result)
 
-        self.word_list = wordlist.split
-    
-    def wordsThatRemain():
-        # use feedback to reduce the overall dictionary to 
-        # make a list of words make a suggestion
+        self.wordlist = wordlist
 
-        pass
-        
-    def wordleFeedback(userGuess,wordleReturn):
+    #############################################################
+    ## uses feedback from the webpage via regular expression to
+    # limit what words remain
+    #############################################################
+
+    def wordleFeedback(self, userGuess, wordleReturn_input):
 
         # use feedback from wordle to remove potential words from
         # the list
-        
+
+        #initialize positional lists of good letters
+        g_pos = []
+        y_pos = []
+        x_pos = []
+
+        # find the locations of green, yellow, and black letters
+        # save letters in lists
+
+        for i in [0,1,2,3,4]:
+            if wordleReturn_input[i] == 'g':
+                g_pos = g_pos + [i]
+                self.g_list.append(wordleReturn_input[i])
+            elif wordleReturn_input[i] == 'y':
+                y_pos = y_pos + [i]#.append(i)
+                self.y_list.append(wordleReturn_input[i])
+            else:
+                x_pos = x_pos + [i]
+                self.x_list.append(wordleReturn_input[i])
+
+        print("g is at locations: {}".format(g_pos))
+        print("y is at locations: {}".format(y_pos))
+        print(". is at locations: {}".format(x_pos))
+
+
+
+#        for letter in wordleReturn_input:
+#            pattern = re.compile('')
+
+    
+    def wordsThatRemain(self):
+    ## use feedback to reduce the overall dictionary to
+    ## make a list of words make a suggestion
+
         pass
+        
+
 
 
     #this is to show julie how this works!!
--- WordleSolver.py
+++ WordleSolver.py
@@ -1,24 +1,40 @@
 ## Wordle Solver
 # Matthew Vaerewyck
 # Started: 24 Feb 2023
+import re
 
 from WordleDic import wordleSolve
 
 # handles user input and reports back and forth to WordleDic.py
-
 userGuess = "slate"     # user's next guess, initialized as "slate"
-availWords = {""}     # dictionary list of available words
-wordleReturn = "....."  # this will be the user's feedback from wordle
+return_pattern = re.compile('[yg.]{5}') #set up the wordle return feedback limits
+Wordle = wordleSolve()
 
-print('-- Fetching word list -- ')
-Wordle = wordleSolve.getWordList
+try:
+    print('-- Fetching word list -- ')
+    Wordle.getWordList()
 
-print("You guessed first: ", userGuess)
-wordleReturn = input("Please input the response from Wordle (using yg.) :")
+except:
+    print("Failure in retrieving list")
 
+print ('world list obtained')
 
-wordleSolve.wordleFeedback(userGuess,wordleReturn)
+while True:
+    print("You guessed first: ", userGuess)
+    while True:
+        wordleReturn_input = input("Please input the response from Wordle (using yg.) :")
+        wordleReturn = re.search(return_pattern,wordleReturn_input)
 
+        if len(wordleReturn_input)>5 or wordleReturn == None:
+            print("Error, incorrect format - g for green, y for yellow, . for black:")
+        else:
+            break
 
-
-
+    if wordleReturn_input == 'ggggg':
+        print('Another succesful solve!')
+        break
+    elif len(Wordle.wordlist) < 1:
+        print("Error! No more potential words remain!")
+        break
+    else:
+         userGuess = Wordle.wordleFeedback(userGuess, wordleReturn_input)
